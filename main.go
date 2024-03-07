9667ace44588f04331fd92525e92e963fe0cd0ac
arch machine <lilith.alanis.255@gmail.com>
2024-03-07 00:49:11 -0600

    go!

--- main.go
+++ main.go
@@ -7,8 +7,11 @@ import (
 
 func main(){
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		fmt.Fprintf(w, "Hello you have requet %s\n", r.URL.Path)
+		fmt.Fprintf(w, "Hello you have requested %s\n", r.URL.Path)
 	})
+
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	var e = http.ListenAndServe(":8000", nil);
 	if e != nil {
